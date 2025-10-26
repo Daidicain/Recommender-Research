40b8f928c309076ebcdc055a59cc4e9dc9b40925
daidicain <skylar982@gmail.com>
2025-10-26 16:54:10 -0600

    minor changes

--- Implementations/config.py
+++ Implementations/config.py
@@ -3,15 +3,15 @@
 # B_VALUES = [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]
 B_VALUES = ["subset"]
 # A_VALUES = [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]
-T_VALUES = [10,100,1000,10000,100000,1000000,10000000,100000000,1000000000 ]
-# T_VALUES = [1000000000]
+# T_VALUES = [10,100,1000,10000,100000,1000000,10000000,100000000,1000000000 ]
+T_VALUES = [1000000,10000000,100000000,1000000000]
 
 '''DATASETS'''
 # DATASET = 'movielens'
-# DATASET = 'netflix'
+DATASET = 'netflix'
 # DATASET = 'amazon'
 # DATASET = 'epinions'
-DATASET = 'myket'
+# DATASET = 'myket'
 
 '''ALGORITHMS'''
 # SAVE_NAME = 'adamic_adar'
@@ -20,10 +20,10 @@ DATASET = 'myket'
 # SAVE_NAME = 'window'
 # SAVE_NAME = 'window_rating'
 # SAVE_NAME = 'window_subgraph'
-# SAVE_NAME = 'windowSubgraph2'
+SAVE_NAME = 'windowSubgraph2'
 # SAVE_NAME = 'clics'
 # SAVE_NAME = 'link_score'
-SAVE_NAME = 'preferential_attachment'
+# SAVE_NAME = 'preferential_attachment'
 # SAVE_NAME = 'temporal'
 # SAVE_NAME = 'time_score'
 
@@ -33,7 +33,7 @@ B = 0.5
 T = 1000000000
 
 '''SYSTEM'''
-CPU_CORES = 40 # number of cores to use
+CPU_CORES = 80 # number of cores to use
 RANDOM_STATE = 45 # keeps tests consistent between runs
 
 
