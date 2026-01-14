392fee2e9e842bd3db1f55777bb7886c4478c6aa
daidicain <skylar982@gmail.com>
2026-01-13 20:11:40 -0700

    fixed epinions

--- Datasets/epinions/create_sample.py
+++ Datasets/epinions/create_sample.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f6e7d1eeb732cf892b71490d0f9fe918a87dbad9e8fbe2292caff6faa137d81b
-size 2593
+oid sha256:272f73b4931b9318997143929ae5f530a364bce571f8884ad6b6f06a0b2cb00d
+size 2678
